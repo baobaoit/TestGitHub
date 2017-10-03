3e401473ca366a379a63aea6208b81e43df0a9c4
baobaoit <quocbao.lehoang@gmail.com>
2017-10-03 14:26:37 +0700

    Thay noi dung label1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -64,9 +64,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(83, 146);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(40, 13);
             this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.label1.Text = "nhãn 1";
             // 
             // button2
             // 
